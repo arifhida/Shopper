314e75ddd16ca44413926f9689cf93ca08145358
areefhida <areef.hiide@gmail.com>
2017-07-05 13:39:33 +0700

    Add project files.

---
+++ src/Shopper.Model/Entities/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopper.Model.Entities
+{
+    public class Role : EntityBase
+    {
+        public string RoleName { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/Shopper.Model/IEntityBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopper.Model
+{
+    public interface IEntityBase
+    {
+        long Id { get; set; }
+        DateTime CreatedDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime ModifiedDate { get; set; }
+        string ModifiedBy { get; set; }
+        bool isActive { get; set; }
+        bool Delete { get; set; }
+    }
+
+    public class EntityBase : IEntityBase
+    {
+        public long Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string ModifiedBy { get; set; }
+        public bool isActive { get; set; }
+        public bool Delete { get; set; }
+    }
+}
