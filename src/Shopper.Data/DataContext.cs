4f9ef416110c47b895cfd30e40016dc1616ae5b6
areefhida <areef.hiide@gmail.com>
2017-07-11 16:20:26 +0700

    s

---
+++ src/Shopper.Data/DataContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Shopper.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopper.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<UserInRole> UserInRoles { get; set; }
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<User>().Property(e => e.UserName).HasMaxLength(150);
+            modelBuilder.Entity<User>().Property(e => e.FullName).HasMaxLength(250);
+            modelBuilder.Entity<User>().Property(e => e.Password).HasMaxLength(256);
+            modelBuilder.Entity<User>().Property(e => e.CreatedDate).ForNpgsqlHasDefaultValueSql("current_timestamp").ValueGeneratedOnAdd();
+            modelBuilder.Entity<User>().Property(e => e.ModifiedDate).ForNpgsqlHasDefaultValueSql("current_timestamp").ValueGeneratedOnAddOrUpdate();
+            modelBuilder.Entity<User>().Property(e => e.isActive).ForNpgsqlHasDefaultValue(true);
+            modelBuilder.Entity<User>().Property(e => e.Salt).HasMaxLength(256);
+            modelBuilder.Entity<User>().HasIndex(e => e.Email).IsUnique(true);
+            modelBuilder.Entity<User>().HasIndex(e => e.UserName).IsUnique(true);
+            modelBuilder.Entity<User>().Ignore(e => e.Delete);
+
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
